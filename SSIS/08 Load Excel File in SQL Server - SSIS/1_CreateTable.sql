ebcd6e2ba16c8e0d697919ad9436284445525956
geethasamynathan <geethasamynathan2011@gmail.com>
2025-08-08 17:27:05 +0530

    export from excel

---
+++ SSIS/08 Load Excel File in SQL Server - SSIS/1_CreateTable.sql
@@ -0,0 +1,9 @@
+drop table PersonData
+CREATE TABLE [PersonData] (
+    [id] float,
+    [first_name] nvarchar(255),
+    [last_name] nvarchar(255),
+    [email] nvarchar(255),
+    [gender] nvarchar(255),
+    [ip_address] nvarchar(255)
+)
